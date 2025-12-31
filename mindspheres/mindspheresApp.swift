04e346412b47a93b0afaa83d4e4842a7396942d8
Mateus Werneck <mateuswerneckelizeu@yahoo.com.br>
2025-12-31 00:13:09 -0300

    Initial Commit

---
+++ mindspheres/mindspheresApp.swift
@@ -0,0 +1,17 @@
+//
+//  mindspheresApp.swift
+//  mindspheres
+//
+//  Created by Mateus Werneck on 31/12/25.
+//
+
+import SwiftUI
+
+@main
+struct mindspheresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
